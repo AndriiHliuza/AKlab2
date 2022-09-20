9a1ecd19865087f4efca58771eefc5f4a3d41193
Andrii Hliuza <98809528+AndriiHliuza@users.noreply.github.com>
2022-09-20 21:12:20 +0300

    initial commit

---
+++ GitLab/calculator/calculator.h
@@ -0,0 +1,12 @@
+class Calculator
+{
+    public:
+        static int Add (int, int);
+        static int Mul (double, double);
+
+};
+// comment 1
+// comment 2
+// comment 3
+// comment 4
+// comment 5
